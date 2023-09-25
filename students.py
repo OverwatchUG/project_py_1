058a018c278fb9a1a6b5b74602736ff4ae3c364f
Waako Shadidu Ismail <123797767+OverwatchUG@users.noreply.github.com>
2023-09-25 23:08:24 +0300

    Create students.py

---
+++ students.py
@@ -0,0 +1,21 @@
+students = []
+number_of_students = int(input("Enter the number of students"))
+
+for i in range(number_of_student):
+  name = input("Name")
+  age = int(input("Age"))
+  program = input("Program:")
+  year_of_study = int(input("Year of study"))
+  subjects[3] = input("Enter subjects")
+  grade[3] = input(input("Enter grades per subject"))
+
+  student = {
+    "name": name,
+    "age": age,
+    "program": program,
+    "year_of_study": year_of_study,
+    "subjects[3]": subjects[3],
+    "grade[3]": grade[3],
+  }
+
+  students.append(student)
